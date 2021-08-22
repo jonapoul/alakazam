b9adc0343c7b8b0cfcbf96e63429bd5e4be197da
Jon Poulton <jpoulton@protonmail.com>
2021-08-22 11:18:04 +0100

    Test auto-publishing on jitpack

---
+++ library/src/main/java/com/jonapoul/extensions/collections/Lazy.kt
@@ -0,0 +1,19 @@
+package com.jonapoul.extensions.collections
+
+
+fun <DataType> lazyListOf(vararg elements: DataType): Lazy<List<DataType>> =
+    LazyCollection { elements.asList() }
+
+fun <DataType> lazyMutableListOf(vararg elements: DataType): Lazy<MutableList<DataType>> =
+    LazyCollection { elements.asList().toMutableList() }
+
+internal class LazyCollection<DataType, CollectionType : Collection<DataType>>(
+    private val factory: () -> CollectionType
+) : Lazy<CollectionType> {
+    private var cached: CollectionType? = null
+
+    override val value: CollectionType
+        get() = cached ?: factory.invoke().also { cached = it }
+
+    override fun isInitialized(): Boolean = cached != null
+}
