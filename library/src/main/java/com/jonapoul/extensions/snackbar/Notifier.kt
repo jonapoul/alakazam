f5a1328ec8dfe0f3123635a8faa1056c13096245
Jon Poulton <jpoulton@protonmail.com>
2021-09-09 20:51:03 +0100

    Updated notifier

--- library/src/main/java/com/jonapoul/extensions/snackbar/Notifier.kt
+++ library/src/main/java/com/jonapoul/extensions/snackbar/Notifier.kt
@@ -5,92 +5,72 @@ import android.graphics.Color
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.jonapoul.extensions.R
 
-interface INotifier {
-    fun success(root: View, message: String)
-    fun success(root: View, @StringRes message: Int)
-    fun info(root: View, message: String)
-    fun info(root: View, @StringRes message: Int)
-    fun warn(root: View, message: String)
-    fun warn(root: View, @StringRes message: Int)
-    fun error(root: View, message: String)
-    fun error(root: View, @StringRes message: Int)
-    fun toast(context: Context, message: String)
-    fun toast(context: Context, @StringRes message: Int)
-}
-
 class Notifier(
+    @ColorRes private val backgroundColour: Int = R.color.notifier_background,
     @ColorRes private val successColour: Int = R.color.notifier_success,
     @ColorRes private val infoColour: Int = R.color.notifier_info,
+    @ColorRes private val cautionColour: Int = R.color.notifier_caution,
     @ColorRes private val warningColour: Int = R.color.notifier_warning,
-    @ColorRes private val errorColour: Int = R.color.notifier_error,
-) : INotifier {
-
-    fun snackbar(
-        root: View,
-        message: String,
-        @ColorRes bgColour: Int,
-        @ColorInt textColour: Int
-    ) {
+) {
+    private fun snackbar(root: View, message: String, @ColorRes actionTextColour: Int) {
         val snackbar = Snackbar.make(root, message, Snackbar.LENGTH_LONG)
-        val view = snackbar.view
-        view.setBackgroundColor(ContextCompat.getColor(view.context, bgColour))
-        val text = view.findViewById<TextView>(R.id.snackbar_text)
-        text.setTextColor(textColour)
+        snackbar.view.setBackgroundResource(backgroundColour)
+        val text = snackbar.view.findViewById<TextView>(R.id.snackbar_text)
         text.maxLines = 20 // don't crop off any longer messages
+        text.setTextColor(Color.WHITE)
         snackbar.setAction(DEFAULT_TEXT, DEFAULT_ACTION)
-        snackbar.setActionTextColor(textColour)
+        snackbar.setActionTextColor(ContextCompat.getColor(root.context, actionTextColour))
         snackbar.show()
     }
 
-    override fun success(root: View, message: String) {
-        snackbar(root, message, successColour, Color.WHITE)
+    fun success(root: View, message: String) {
+        snackbar(root, message, successColour)
     }
 
-    override fun success(root: View, message: Int) {
+    fun success(root: View, @StringRes message: Int) {
         success(root, root.context.getString(message))
     }
 
-    override fun info(root: View, message: String) {
-        snackbar(root, message, infoColour, Color.BLACK)
+    fun caution(root: View, message: String) {
+        snackbar(root, message, cautionColour)
     }
 
-    override fun info(root: View, message: Int) {
+    fun caution(root: View, @StringRes message: Int) {
+        caution(root, root.context.getString(message))
+    }
+
+    fun warning(root: View, message: String) {
+        snackbar(root, message, warningColour)
+    }
+
+    fun warning(root: View, @StringRes message: Int) {
+        caution(root, root.context.getString(message))
+    }
+
+    fun info(root: View, message: String) {
+        snackbar(root, message, infoColour)
+    }
+
+    fun info(root: View, @StringRes message: Int) {
         info(root, root.context.getString(message))
     }
 
-    override fun warn(root: View, message: String) {
-        snackbar(root, message, warningColour, Color.BLACK)
-    }
-
-    override fun warn(root: View, message: Int) {
-        warn(root, root.context.getString(message))
-    }
-
-    override fun error(root: View, message: String) {
-        snackbar(root, message, errorColour, Color.WHITE)
-    }
-
-    override fun error(root: View, message: Int) {
-        error(root, root.context.getString(message))
-    }
-
-    override fun toast(context: Context, message: String) {
+    fun toast(context: Context, message: String) {
         Toast.makeText(context.applicationContext, message, Toast.LENGTH_LONG).show()
     }
 
-    override fun toast(context: Context, message: Int) {
+    fun toast(context: Context, @StringRes message: Int) {
         toast(context, context.getString(message))
     }
 
-    private companion object {
-        const val DEFAULT_TEXT = "OK"
-        val DEFAULT_ACTION = View.OnClickListener { /* No-op */ }
+    companion object {
+        private const val DEFAULT_TEXT = "OK"
+        private val DEFAULT_ACTION = View.OnClickListener { /* No-op */ }
     }
 }
