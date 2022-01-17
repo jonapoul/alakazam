04a0e8807811fb9bcae71dbd3605389dac4e5510
Jon Poulton <jpoulton@protonmail.com>
2022-01-17 20:57:13 +0000

    Updated dialog classes/functions

---
+++ library/src/main/java/com/jonapoul/extensions/dialogs/DarkenedBackgroundAlertDialogBuilder.kt
@@ -0,0 +1,28 @@
+package com.jonapoul.extensions.dialogs
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+/**
+ * Simple wrapper class to apply a darkened background to any alert dialog popups. Note that this
+ * doesn't apply to preference dialogs.
+ */
+open class DarkenedBackgroundAlertDialogBuilder(
+    context: Context,
+    overrideThemeResId: Int = 0
+) : MaterialAlertDialogBuilder(
+    context,
+    overrideThemeResId
+) {
+    override fun show(): AlertDialog {
+        val dialog = create()
+        dialog.window?.setDimAmount(DIM_AMOUNT)
+        dialog.show()
+        return dialog
+    }
+
+    private companion object {
+        const val DIM_AMOUNT = 0.8f
+    }
+}
--- library/src/main/java/com/jonapoul/extensions/dialogs/MaterialAlertDialogBuilderExtensions.kt
+++ library/src/main/java/com/jonapoul/extensions/dialogs/MaterialAlertDialogBuilderExtensions.kt
@@ -1,33 +1,36 @@
 package com.jonapoul.extensions.dialogs
 
+import androidx.annotation.StringRes
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
  * Gives a best-case API to allow callbacks on pressing the "negative" button in a dialog. Here we
- * apply a default (system-translated) string of "CANCEL" and invoke the supplied [call] when it's
- * pressed. If [call] is null, the button will simply close the dialog.
+ * apply a default (system-translated) string of "CANCEL" and invoke the supplied [onClick] when it's
+ * pressed. If [onClick] is null, the button will simply close the dialog.
  */
 fun MaterialAlertDialogBuilder.setSimpleNegativeButton(
-    call: (() -> Unit)? = null
+    @StringRes text: Int = android.R.string.cancel,
+    onClick: (() -> Unit)? = null
 ): MaterialAlertDialogBuilder {
-    return if (call == null) {
-        setNegativeButton(android.R.string.cancel, null)
+    return if (onClick == null) {
+        setNegativeButton(text, null)
     } else {
-        setNegativeButton(android.R.string.cancel) { _, _ -> call.invoke() }
+        setNegativeButton(text) { _, _ -> onClick.invoke() }
     }
 }
 
 /**
  * Gives a best-case API to allow callbacks on pressing the "positive" button in a dialog. Here we
- * apply a default (system-translated) string of "OK" and invoke the supplied [call] when it's
- * pressed. If [call] is null, the button will simply close the dialog.
+ * apply a default (system-translated) string of "OK" and invoke the supplied [onClick] when it's
+ * pressed. If [onClick] is null, the button will simply close the dialog.
  */
 fun MaterialAlertDialogBuilder.setSimplePositiveButton(
-    call: (() -> Unit)? = null
+    @StringRes text: Int = android.R.string.ok,
+    onClick: (() -> Unit)? = null
 ): MaterialAlertDialogBuilder {
-    return if (call == null) {
-        setPositiveButton(android.R.string.ok, null)
+    return if (onClick == null) {
+        setPositiveButton(text, null)
     } else {
-        setPositiveButton(android.R.string.ok) { _, _ -> call.invoke() }
+        setPositiveButton(text) { _, _ -> onClick.invoke() }
     }
 }
