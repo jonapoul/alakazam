d7ec6ad02ef3f0ebb16db7d6390e76c1614bc161
Jon Poulton <jpoulton@protonmail.com>
2022-01-23 20:57:52 +0000

    Updated DispatcherProvider

--- library/src/main/java/com/jonapoul/extensions/coroutines/DispatcherProvider.kt
+++ library/src/main/java/com/jonapoul/extensions/coroutines/DispatcherProvider.kt
@@ -1,9 +1,10 @@
 package com.jonapoul.extensions.coroutines
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Inject
 
 class DispatcherProvider @Inject constructor() {
-    fun main() = Dispatchers.Main
-    fun io() = Dispatchers.IO
+    fun main(): CoroutineDispatcher = Dispatchers.Main
+    fun io(): CoroutineDispatcher = Dispatchers.IO
 }
