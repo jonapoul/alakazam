d1094fd85fd168e57096568b1dcbc4dc900758e9
Jon Poulton <jpoulton@protonmail.com>
2022-02-25 18:48:37 +0000

    Fixed build and added common Hilt modules

--- sample/src/main/java/com/jonapoul/extensions/sample/SampleApplication.kt
+++ sample/src/main/java/com/jonapoul/extensions/sample/SampleApplication.kt
@@ -1,23 +1,5 @@
 package com.jonapoul.extensions.sample
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.preference.PreferenceManager
-import com.jonapoul.extensions.domain.getIntFromPair
-import com.jonapoul.extensions.ui.dialogs.setDialogThemes
 
-class SampleApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        AppCompatDelegate.setDefaultNightMode(
-            prefs.getIntFromPair(Prefs.APP_THEME)
-        )
-
-        setDialogThemes(
-            cautionTheme = R.style.Theme_Sample_AlertDialog_Caution,
-            warningTheme = R.style.Theme_Sample_AlertDialog_Warning
-        )
-    }
-}
+class SampleApplication : Application()
