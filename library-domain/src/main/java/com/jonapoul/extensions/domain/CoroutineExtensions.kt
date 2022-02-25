f6e2e024c7f776e1ae4e63775d52bd56032bc9dd
Jon Poulton <jpoulton@protonmail.com>
2022-02-25 18:36:45 +0000

    Renamed infiniteLoop to launchInfiniteLoop

--- library-domain/src/main/java/com/jonapoul/extensions/domain/CoroutineExtensions.kt
+++ library-domain/src/main/java/com/jonapoul/extensions/domain/CoroutineExtensions.kt
@@ -14,27 +14,27 @@ import kotlinx.coroutines.launch
  * callback or after. This will only matter on the first run through. The [skipIf] lambda will be
  * run directly before invoking the main [call], to verify whether the [call] should be run.
  */
-fun CoroutineScope.infiniteLoop(
+fun CoroutineScope.launchInfiniteLoop(
     periodMs: Long,
     delayType: LoopDelayType = LoopDelayType.DELAY_AFTER,
     dispatcher: CoroutineDispatcher = Dispatchers.Main,
     skipIf: (() -> Boolean)? = null,
     call: suspend () -> Unit,
 ): Job {
-    val delayBefore = if (delayType == LoopDelayType.DELAY_BEFORE) periodMs else 0L
-    val delayAfter = if (delayType == LoopDelayType.DELAY_AFTER) periodMs else 0L
+    val delayBefore = delayType == LoopDelayType.DELAY_BEFORE
+    val delayAfter = delayType == LoopDelayType.DELAY_AFTER
     return launch(dispatcher) {
         while (true) {
-            delay(delayBefore)
+            if (delayBefore) delay(periodMs)
             if (skipIf?.invoke() == true) continue
             call.invoke()
-            delay(delayAfter)
+            if (delayAfter) delay(periodMs)
         }
     }
 }
 
 /**
- * Defines the time to perform the sleep operation on our looping coroutine in [infiniteLoop].
+ * Defines the time to perform the sleep operation on our looping coroutine in [launchInfiniteLoop].
  */
 @Keep
 enum class LoopDelayType { DELAY_BEFORE, DELAY_AFTER }
