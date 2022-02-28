833b06372375d42289b9951260983a644d7c8628
Jon Poulton <jpoulton@protonmail.com>
2022-02-28 20:05:28 +0000

    Updated CorePreferenceFragment

--- lib-ui/src/main/kotlin/com/jonapoul/common/ui/CorePreferenceFragment.kt
+++ lib-ui/src/main/kotlin/com/jonapoul/common/ui/CorePreferenceFragment.kt
@@ -1,16 +1,41 @@
 package com.jonapoul.common.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.CallSuper
+import androidx.annotation.MenuRes
 import androidx.annotation.XmlRes
 import androidx.preference.PreferenceFragmentCompat
 
 abstract class CorePreferenceFragment(
-    @XmlRes private val preferenceXmlRes: Int,
+    @XmlRes private val settings: Int,
+    @MenuRes private val menu: Int?,
 ) : PreferenceFragmentCompat() {
+
+    protected val navController by navControllers()
 
     @CallSuper
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(preferenceXmlRes, null)
+        setPreferencesFromResource(settings, null)
+    }
+
+    @CallSuper
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        setHasOptionsMenu(menu != null)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    @CallSuper
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        this.menu?.let { inflater.inflate(it, menu) }
     }
 }
