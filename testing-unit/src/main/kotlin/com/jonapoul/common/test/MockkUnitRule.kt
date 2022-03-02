fc43df61f4c724a555cad653f49ff065609b9bc3
Jon Poulton <jpoulton@protonmail.com>
2022-03-02 20:21:41 +0000

    Renamed some classes for dependency resolution purposes

--- testing-unit/src/main/kotlin/com/jonapoul/common/test/MockkUnitRule.kt
+++ testing-unit/src/main/kotlin/com/jonapoul/common/test/MockkUnitRule.kt
@@ -2,10 +2,16 @@ package com.jonapoul.common.test
 
 import io.mockk.MockKAnnotations
 import io.mockk.unmockkAll
+import org.junit.rules.TestRule
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
-class MockkRule(private val subject: Any) : TestWatcher() {
+/**
+ * Handles the initialisation and tearing down of mock objects using the @MockK or @RelaxedMockK
+ * annotations. Each mock will be re-initialised and torn down at the beginning and end of each
+ * test.
+ */
+class MockkUnitRule(private val subject: Any) : TestWatcher(), TestRule {
     override fun starting(description: Description?) {
         super.starting(description)
         MockKAnnotations.init(subject)
