850047369f949567f5113325d6c6a99cc184ea6b
Jon Poulton <jpoulton@protonmail.com>
2022-03-17 18:30:50 +0000

    Added ability to auto-clear RecyclerViews

--- lib-ui/src/main/kotlin/com/jonapoul/common/ui/CommonFragment.kt
+++ lib-ui/src/main/kotlin/com/jonapoul/common/ui/CommonFragment.kt
@@ -3,10 +3,14 @@ package com.jonapoul.common.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.CallSuper
 import androidx.annotation.LayoutRes
 import androidx.annotation.MenuRes
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
 abstract class CommonFragment(
@@ -16,6 +20,7 @@ abstract class CommonFragment(
 
     protected abstract val binding: ViewBinding
     protected val navController by navControllers()
+    private val recyclerViews = mutableListOf<RecyclerView>()
 
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +32,24 @@ abstract class CommonFragment(
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         this.menu?.let { inflater.inflate(it, menu) }
+    }
+
+    @CallSuper
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        grabRecyclerViews(view)
+    }
+
+    @CallSuper
+    override fun onDestroyView() {
+        super.onDestroyView()
+        recyclerViews.forEach { it.adapter = null }
+        recyclerViews.clear()
+    }
+
+    private fun grabRecyclerViews(view: View) {
+        when (view) {
+            is RecyclerView -> recyclerViews.add(view)
+            is ViewGroup -> view.children.forEach(::grabRecyclerViews)
+        }
     }
 }
