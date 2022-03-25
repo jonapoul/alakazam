1a1fa4893a23b2463aa9d1d29b2fac2ea149fc17
Jon Poulton <jpoulton@protonmail.com>
2022-03-25 20:00:04 +0000

    Run logging initialiser before all others

--- lib-domain/src/main/kotlin/com/jonapoul/common/domain/init/AppInitialisers.kt
+++ lib-domain/src/main/kotlin/com/jonapoul/common/domain/init/AppInitialisers.kt
@@ -7,6 +7,14 @@ class AppInitialisers @Inject constructor(
     private val initialisers: Set<@JvmSuppressWildcards IAppInitialiser>,
 ) {
     fun init(app: Application) {
-        initialisers.forEach { it.init(app) }
+        /* Begin logging first, before the others */
+        initialisers
+            .filterIsInstance<LoggingInitialiser>()
+            .forEach { it.init(app) }
+
+        /* Then run all other initialisers */
+        initialisers
+            .filterNot { it is LoggingInitialiser }
+            .forEach { it.init(app) }
     }
 }
