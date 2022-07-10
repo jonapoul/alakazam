9ca414b006a6eed300fc7e3672d8ac196e636b74
Jon Poulton <jpoulton@protonmail.com>
2022-07-10 11:09:54 +0100

    Update MaterialAlertDialogBuilderExtensions.kt

--- lib-ui/src/main/kotlin/com/jonapoul/common/ui/dialogs/MaterialAlertDialogBuilderExtensions.kt
+++ lib-ui/src/main/kotlin/com/jonapoul/common/ui/dialogs/MaterialAlertDialogBuilderExtensions.kt
@@ -1,21 +1,23 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.jonapoul.common.ui.dialogs
 
 import androidx.annotation.StringRes
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import androidx.appcompat.app.AlertDialog
 
 /**
  * Gives a best-case API to allow callbacks on pressing the "negative" button in a dialog. Here we
  * apply a default (system-translated) string of "CANCEL" and invoke the supplied [onClick] when it's
  * pressed. If [onClick] is null, the button will simply close the dialog.
  */
-fun MaterialAlertDialogBuilder.setSimpleNegativeButton(
+fun <T : AlertDialog.Builder> T.setSimpleNegativeButton(
     @StringRes text: Int = android.R.string.cancel,
     onClick: (() -> Unit)? = null,
-): MaterialAlertDialogBuilder {
+): T {
     return if (onClick == null) {
-        setNegativeButton(text, null)
+        setNegativeButton(text, null) as T
     } else {
-        setNegativeButton(text) { _, _ -> onClick.invoke() }
+        setNegativeButton(text) { _, _ -> onClick.invoke() } as T
     }
 }
 
@@ -24,24 +26,24 @@ fun MaterialAlertDialogBuilder.setSimpleNegativeButton(
  * apply a default (system-translated) string of "OK" and invoke the supplied [onClick] when it's
  * pressed. If [onClick] is null, the button will simply close the dialog.
  */
-fun MaterialAlertDialogBuilder.setSimplePositiveButton(
+fun <T : AlertDialog.Builder> T.setSimplePositiveButton(
     @StringRes text: Int = android.R.string.ok,
     onClick: (() -> Unit)? = null,
-): MaterialAlertDialogBuilder {
+): T {
     return if (onClick == null) {
-        setPositiveButton(text, null)
+        setPositiveButton(text, null) as T
     } else {
-        setPositiveButton(text) { _, _ -> onClick.invoke() }
+        setPositiveButton(text) { _, _ -> onClick.invoke() } as T
     }
 }
 
-fun MaterialAlertDialogBuilder.setSimpleNeutralButton(
+fun <T : AlertDialog.Builder> T.setSimpleNeutralButton(
     @StringRes text: Int,
     onClick: (() -> Unit)? = null,
-): MaterialAlertDialogBuilder {
+): T {
     return if (onClick == null) {
-        setNeutralButton(text, null)
+        setNeutralButton(text, null) as T
     } else {
-        setNeutralButton(text) { _, _ -> onClick.invoke() }
+        setNeutralButton(text) { _, _ -> onClick.invoke() } as T
     }
 }
