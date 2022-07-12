ab58b66d8fa747b339d06258e25f9dad8231c4ae
Jon Poulton <jpoulton@protonmail.com>
2022-07-12 21:14:43 +0100

    Added optional callback on snackbars

--- lib-ui/src/main/kotlin/com/jonapoul/common/ui/SnackbarMessage.kt
+++ lib-ui/src/main/kotlin/com/jonapoul/common/ui/SnackbarMessage.kt
@@ -4,8 +4,23 @@ import android.view.View
 import com.jonapoul.common.ui.notifier.Notifier
 
 sealed class SnackbarMessage(val notify: Notifier.(View) -> Unit) {
-    data class Info(val message: String) : SnackbarMessage({ v -> info(v, message) })
-    data class Success(val message: String) : SnackbarMessage({ v -> success(v, message) })
-    data class Caution(val message: String) : SnackbarMessage({ v -> caution(v, message) })
-    data class Warning(val message: String) : SnackbarMessage({ v -> warning(v, message) })
+    data class Info(
+        val message: String,
+        val action: Notifier.Action = Notifier.EMPTY_ACTION
+    ) : SnackbarMessage({ v -> info(v, message, action) })
+
+    data class Success(
+        val message: String,
+        val action: Notifier.Action = Notifier.EMPTY_ACTION
+    ) : SnackbarMessage({ v -> success(v, message, action) })
+
+    data class Caution(
+        val message: String,
+        val action: Notifier.Action = Notifier.EMPTY_ACTION
+    ) : SnackbarMessage({ v -> caution(v, message, action) })
+
+    data class Warning(
+        val message: String,
+        val action: Notifier.Action = Notifier.EMPTY_ACTION
+    ) : SnackbarMessage({ v -> warning(v, message, action) })
 }
--- lib-ui/src/main/kotlin/com/jonapoul/common/ui/notifier/Notifier.kt
+++ lib-ui/src/main/kotlin/com/jonapoul/common/ui/notifier/Notifier.kt
@@ -16,52 +16,56 @@ class Notifier(
     @ColorRes private val cautionColour: Int = R.color.notifierCaution,
     @ColorRes private val warningColour: Int = R.color.notifierWarning,
 ) {
-    private fun snackbar(root: View, message: String, @ColorRes actionTextColour: Int) {
+    data class Action(
+        @StringRes val actionText: Int,
+        val onClick: View.OnClickListener,
+    )
+
+    private fun snackbar(root: View, message: String, @ColorRes actionTextColour: Int, action: Action) {
         val snackbar = Snackbar.make(root, message, Snackbar.LENGTH_LONG)
         snackbar.view.setBackgroundResource(backgroundColour)
         val text = snackbar.view.findViewById<TextView>(R.id.snackbar_text)
         text.maxLines = MAX_LINES // don't crop off any longer messages
         text.setTextColor(Color.WHITE)
-        snackbar.setAction(DEFAULT_TEXT, DEFAULT_ACTION)
+        snackbar.setAction(action.actionText, action.onClick)
         snackbar.setActionTextColor(ContextCompat.getColor(root.context, actionTextColour))
         snackbar.show()
     }
 
-    fun success(root: View, message: String) {
-        snackbar(root, message, successColour)
+    fun success(root: View, message: String, action: Action = EMPTY_ACTION) {
+        snackbar(root, message, successColour, action)
     }
 
-    fun success(root: View, @StringRes message: Int) {
-        success(root, root.context.getString(message))
+    fun success(root: View, @StringRes message: Int, action: Action = EMPTY_ACTION) {
+        success(root, root.context.getString(message), action)
     }
 
-    fun caution(root: View, message: String) {
-        snackbar(root, message, cautionColour)
+    fun caution(root: View, message: String, action: Action = EMPTY_ACTION) {
+        snackbar(root, message, cautionColour, action)
     }
 
-    fun caution(root: View, @StringRes message: Int) {
-        caution(root, root.context.getString(message))
+    fun caution(root: View, @StringRes message: Int, action: Action = EMPTY_ACTION) {
+        caution(root, root.context.getString(message), action)
     }
 
-    fun warning(root: View, message: String) {
-        snackbar(root, message, warningColour)
+    fun warning(root: View, message: String, action: Action = EMPTY_ACTION) {
+        snackbar(root, message, warningColour, action)
     }
 
-    fun warning(root: View, @StringRes message: Int) {
-        caution(root, root.context.getString(message))
+    fun warning(root: View, @StringRes message: Int, action: Action = EMPTY_ACTION) {
+        caution(root, root.context.getString(message), action)
     }
 
-    fun info(root: View, message: String) {
-        snackbar(root, message, infoColour)
+    fun info(root: View, message: String, action: Action = EMPTY_ACTION) {
+        snackbar(root, message, infoColour, action)
     }
 
-    fun info(root: View, @StringRes message: Int) {
-        info(root, root.context.getString(message))
+    fun info(root: View, @StringRes message: Int, action: Action = EMPTY_ACTION) {
+        info(root, root.context.getString(message), action)
     }
 
     companion object {
-        private const val DEFAULT_TEXT = "OK"
         private const val MAX_LINES = 20
-        private val DEFAULT_ACTION = View.OnClickListener { /* No-op */ }
+        val EMPTY_ACTION = Action(android.R.string.ok) { /* No-op */ }
     }
 }
