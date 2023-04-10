abe18cd2bcc273c23767519c1a88723be52d1778
Jon Poulton <jpoulton@protonmail.com>
2023-04-10 09:31:36 +0100

    Upversion

--- buildSrc/src/main/kotlin/BuildConstants.kt
+++ buildSrc/src/main/kotlin/BuildConstants.kt
@@ -2,6 +2,6 @@ object BuildConstants {
   const val MIN_SDK = 15
   const val COMPILE_SDK = 33
 
-  const val LIB_VERSION_NAME = "3.0.0-rc03"
-  const val LIB_VERSION_CODE = 202304091
+  const val LIB_VERSION_NAME = "3.0.0"
+  const val LIB_VERSION_CODE = 202304100
 }
