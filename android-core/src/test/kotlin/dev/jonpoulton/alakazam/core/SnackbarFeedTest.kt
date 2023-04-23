88c8e8e54fa67148d1bc9bcb3db258c22e707abb
Jon Poulton <jpoulton@protonmail.com>
2023-04-23 12:14:46 +0100

    Update SnackbarFeedTest.kt

--- android-core/src/test/kotlin/dev/jonpoulton/alakazam/core/SnackbarFeedTest.kt
+++ android-core/src/test/kotlin/dev/jonpoulton/alakazam/core/SnackbarFeedTest.kt
@@ -27,7 +27,7 @@ class SnackbarFeedTest {
   }
 
   @Test
-  fun `One message emitted`() = runTest {
+  fun `One message emitted after add`() = runTest {
     val message = "Hello world"
     val snackbarMessage = SnackbarMessage.Success(message)
     snackbarFeed.add(snackbarMessage)
@@ -48,7 +48,7 @@ class SnackbarFeedTest {
   }
 
   @Test
-  fun `Multiple messages emitted in sequence`() = runTest {
+  fun `Multiple messages emitted in sequence after add`() = runTest {
     snackbarFeed.snackbars.test {
       /* The added messages are emitted in sequence */
       assertNull(awaitItem())
@@ -65,6 +65,54 @@ class SnackbarFeedTest {
 
       assertNull(awaitItem())
       snackbarFeed.add(SnackbarMessage.Warning("WARNING"))
+      assertIs<SnackbarMessage.Warning>(awaitItem())
+
+      /* Null is emitted afterwards */
+      assertNull(awaitItem())
+
+      cancelAndIgnoreRemainingEvents()
+    }
+  }
+
+  @Test
+  fun `One message emitted after post`() = runTest {
+    val message = "Hello world"
+    val snackbarMessage = SnackbarMessage.Success(message)
+    snackbarFeed.post(snackbarMessage)
+
+    snackbarFeed.snackbars.test {
+      assertNull(awaitItem())
+
+      /* The added message is emitted */
+      val emitted = awaitItem()
+      assertIs<SnackbarMessage.Success>(emitted)
+      assertEquals(expected = message, actual = emitted.message)
+
+      /* Null is emitted afterwards */
+      assertNull(awaitItem())
+
+      cancelAndIgnoreRemainingEvents()
+    }
+  }
+
+  @Test
+  fun `Multiple messages emitted in sequence after post`() = runTest {
+    snackbarFeed.snackbars.test {
+      /* The added messages are emitted in sequence */
+      assertNull(awaitItem())
+      snackbarFeed.post(SnackbarMessage.Info("INFO"))
+      assertIs<SnackbarMessage.Info>(awaitItem())
+
+      assertNull(awaitItem())
+      snackbarFeed.post(SnackbarMessage.Success("SUCCESS"))
+      assertIs<SnackbarMessage.Success>(awaitItem())
+
+      assertNull(awaitItem())
+      snackbarFeed.post(SnackbarMessage.Caution("CAUTION"))
+      assertIs<SnackbarMessage.Caution>(awaitItem())
+
+      assertNull(awaitItem())
+      snackbarFeed.post(SnackbarMessage.Warning("WARNING"))
       assertIs<SnackbarMessage.Warning>(awaitItem())
 
       /* Null is emitted afterwards */
