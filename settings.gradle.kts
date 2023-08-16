15fd076500d57637d637d678f8424e58a368dbe0
Jon Poulton <jpoulton@protonmail.com>
2023-08-16 18:54:44 +0100

    Remove testing-unit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -42,7 +42,6 @@ include(":testing-android")
 include(":testing-core")
 include(":testing-db")
 include(":testing-hilt")
-include(":testing-unit")
 
 // https://docs.gradle.org/current/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
