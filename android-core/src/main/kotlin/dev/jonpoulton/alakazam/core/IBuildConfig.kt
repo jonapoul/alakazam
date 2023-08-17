a1cea21bc33981132972391b1a5d057da034e9eb
Jon Poulton <jpoulton@protonmail.com>
2023-08-17 23:30:34 +0100

    Expanded IBuildConfig

--- android-core/src/main/kotlin/dev/jonpoulton/alakazam/core/IBuildConfig.kt
+++ android-core/src/main/kotlin/dev/jonpoulton/alakazam/core/IBuildConfig.kt
@@ -1,5 +1,7 @@
 package dev.jonpoulton.alakazam.core
 
+import android.annotation.SuppressLint
+import android.os.Build
 import java.time.Instant
 
 /**
@@ -14,8 +16,37 @@ interface IBuildConfig {
   val applicationId: String // BuildConfig.APPLICATION_ID
   val gitId: String // custom build config field
   val buildTime: Instant // custom build config field
-  val os: Int // Build.VERSION.SDK_INT
   val platform: String // name of your app
+  val os: Int // Build.VERSION.SDK_INT
   val manufacturer: String // Build.MANUFACTURER
   val model: String // Build.MODEL
+  val repoName: String // e.g. "jonapoul/alakazam"
+  val repoUrl: String // e.g. "https://github.com/jonapoul/alakazam"
+}
+
+@Suppress("LongParameterList")
+@SuppressLint("AnnotateVersionCheck")
+fun buildConfig(
+  debug: Boolean,
+  versionName: String,
+  versionCode: Int,
+  applicationId: String,
+  gitId: String,
+  buildTime: Instant,
+  platform: String,
+  repoName: String,
+  repoUrl: String,
+): IBuildConfig = object : IBuildConfig {
+  override val debug = debug
+  override val versionName = versionName
+  override val versionCode = versionCode
+  override val applicationId = applicationId
+  override val gitId = gitId
+  override val buildTime = buildTime
+  override val platform = platform
+  override val os = Build.VERSION.SDK_INT
+  override val manufacturer = Build.MANUFACTURER
+  override val model = Build.MODEL
+  override val repoName = repoName
+  override val repoUrl = repoUrl
 }
