79a56cfa81cb741e727929225b687ffaeb9b67eb
Jon Poulton <jpoulton@pm.me>
2023-08-19 20:43:29 +0100

    Removed viewbinding from tak-core

--- tak-core/src/main/kotlin/dev/jonpoulton/alakazam/tak/core/AboutDialog.kt
+++ tak-core/src/main/kotlin/dev/jonpoulton/alakazam/tak/core/AboutDialog.kt
@@ -4,10 +4,13 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import com.atakmap.android.maps.MapView
 import dev.jonpoulton.alakazam.core.IBuildConfig
 import dev.jonpoulton.alakazam.core.inflater
-import dev.jonpoulton.alakazam.tak.core.databinding.DialogAboutBinding
 import dev.jonpoulton.alakazam.time.localisedFormatter
 import dev.jonpoulton.alakazam.ui.core.show
 
@@ -19,7 +22,8 @@ object AboutDialog {
     showImpl(mapView.context, pluginContext, buildConfig)
 
   private fun showImpl(appContext: Context, pluginContext: PluginContext, buildConfig: IBuildConfig): AlertDialog {
-    val binding = DialogAboutBinding.inflate(pluginContext.inflater).apply {
+    val view = pluginContext.inflater.inflate(R.layout.dialog_about, null, false)
+    AboutBinding(view).apply {
       version.icon.setImageResource(R.drawable.ic_version)
       version.title.setText(R.string.about_version)
       version.subtitle.text = buildConfig.versionName
@@ -41,8 +45,34 @@ object AboutDialog {
     }
     return PluginAlertDialogBuilder(appContext, pluginContext)
       .setTitle(R.string.about_title)
-      .setView(binding.root)
+      .setView(view)
       .setSimplePositiveButton()
       .show()
+  }
+
+  private class AboutBinding(
+    val version: AboutItemBinding,
+    val buildTime: AboutItemBinding,
+    val source: AboutItemBinding,
+  ) {
+    constructor(view: View) : this(
+      version = AboutItemBinding(view.findViewById(R.id.version)),
+      buildTime = AboutItemBinding(view.findViewById(R.id.build_time)),
+      source = AboutItemBinding(view.findViewById(R.id.source)),
+    )
+  }
+
+  private class AboutItemBinding(
+    val icon: ImageView,
+    val title: TextView,
+    val subtitle: TextView,
+    val launchButton: Button,
+  ) {
+    constructor(view: View) : this(
+      icon = view.findViewById(R.id.icon),
+      title = view.findViewById(R.id.title),
+      subtitle = view.findViewById(R.id.subtitle),
+      launchButton = view.findViewById(R.id.launch_button),
+    )
   }
 }
