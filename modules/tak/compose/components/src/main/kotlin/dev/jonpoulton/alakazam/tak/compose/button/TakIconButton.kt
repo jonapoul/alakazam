1a59c774b686dc92a46afd1e3adf13432a7dbfe0
Jon Poulton <jpoulton@pm.me>
2024-01-17 21:13:16 +0000

    Long pressable TakIconButton

--- modules/tak/compose/components/src/main/kotlin/dev/jonpoulton/alakazam/tak/compose/button/TakIconButton.kt
+++ modules/tak/compose/components/src/main/kotlin/dev/jonpoulton/alakazam/tak/compose/button/TakIconButton.kt
@@ -1,24 +1,33 @@
 package dev.jonpoulton.alakazam.tak.compose.button
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.unit.dp
 import dev.jonpoulton.alakazam.android.ui.compose.PreviewDark
 import dev.jonpoulton.alakazam.tak.compose.core.TakColors
-import dev.jonpoulton.alakazam.tak.compose.core.TakLegacyColors
 import dev.jonpoulton.alakazam.tak.compose.core.TakToast
 import dev.jonpoulton.alakazam.tak.compose.icons.TakIcons
 import dev.jonpoulton.alakazam.tak.compose.icons.sidemenu.Add
@@ -31,58 +40,87 @@ import dev.jonpoulton.alakazam.tak.compose.preview.TakPreview
 public fun TakIconButton(
   icon: ImageVector,
   contentDescription: String,
+  modifier: Modifier = Modifier,
   isError: Boolean = false,
   isDisabled: Boolean = false,
-  colors: TakButtonColors = defaultTakIconButtonColors(),
+  shape: Shape = RoundedShape,
+  colors: TakButtonColors = DefaultTakIconButtonColors(),
   interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
   onClick: () -> Unit,
 ) {
   val isPressed by interactionSource.collectIsPressedAsState()
   val backgroundColor by colors.backgroundColor(!isDisabled, isPressed, isError)
+  val foregroundColor by colors.foregroundColor(!isDisabled, isPressed, isError)
 
   var isLongPressed by remember { mutableStateOf(false) }
+  val haptics = LocalHapticFeedback.current
+
+  Box(
+    modifier = modifier.background(backgroundColor, shape),
+  ) {
+    Image(
+      modifier = Modifier
+        .padding(5.dp)
+        .combinedClickable(
+          interactionSource,
+          enabled = !isDisabled,
+          onClick = onClick,
+          onLongClick = {
+            haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+            isLongPressed = true
+          },
+          indication = null,
+        ),
+      imageVector = icon,
+      contentDescription = contentDescription,
+      colorFilter = ColorFilter.tint(foregroundColor),
+    )
+  }
+
   if (isLongPressed) {
     TakToast(text = contentDescription)
     isLongPressed = false
   }
-
-  Image(
-    modifier = Modifier
-      .padding(5.dp)
-      .combinedClickable(
-        interactionSource,
-        enabled = !isDisabled,
-        onClick = onClick,
-        onLongClick = { isLongPressed = true },
-        indication = null,
-      ),
-    imageVector = icon,
-    contentDescription = contentDescription,
-    colorFilter = ColorFilter.tint(backgroundColor),
-  )
 }
 
+private val RoundedShape = RoundedCornerShape(corner = CornerSize(5.dp))
+
 @Stable
-@Composable
-public fun defaultTakIconButtonColors(
-  normalBackgroundColor: Color = TakColors.Sand,
-  pressedBackgroundColor: Color = TakColors.Cyber,
-  errorBackgroundColor: Color = TakColors.Alert,
-  disabledBackgroundColor: Color = TakColors.Ash,
-  normalForegroundColor: Color = TakColors.Ink,
-  pressedForegroundColor: Color = normalForegroundColor,
-  errorForegroundColor: Color = normalForegroundColor,
-  disabledForegroundColor: Color = TakLegacyColors.Gray,
-): TakButtonColors = DefaultTakButtonColors(
-  normalBackgroundColor = normalBackgroundColor,
-  pressedBackgroundColor = pressedBackgroundColor,
-  errorBackgroundColor = errorBackgroundColor,
-  disabledBackgroundColor = disabledBackgroundColor,
-  normalForegroundColor = normalForegroundColor,
-  pressedForegroundColor = pressedForegroundColor,
-  errorForegroundColor = errorForegroundColor,
-  disabledForegroundColor = disabledForegroundColor,
-)
+@Immutable
+public data class DefaultTakIconButtonColors(
+  val normalBackgroundColor: Color = Color.Transparent,
+  val pressedBackgroundColor: Color = TakColors.Ash,
+  val errorBackgroundColor: Color = TakColors.Charcoal,
+  val disabledBackgroundColor: Color = TakColors.Charcoal,
+  val normalForegroundColor: Color = TakColors.Sand,
+  val pressedForegroundColor: Color = TakColors.Cyber,
+  val errorForegroundColor: Color = TakColors.Alert,
+  val disabledForegroundColor: Color = TakColors.Ash,
+) : TakButtonColors {
+  @Stable
+  @Composable
+  override fun backgroundColor(enabled: Boolean, pressed: Boolean, error: Boolean): State<Color> =
+    rememberUpdatedState(
+      newValue = when {
+        !enabled -> disabledBackgroundColor
+        pressed -> pressedBackgroundColor
+        error -> errorBackgroundColor
+        else -> normalBackgroundColor
+      },
+    )
+
+  @Stable
+  @Composable
+  override fun foregroundColor(enabled: Boolean, pressed: Boolean, error: Boolean): State<Color> =
+    rememberUpdatedState(
+      newValue = when {
+        !enabled -> disabledForegroundColor
+        pressed -> pressedForegroundColor
+        error -> errorForegroundColor
+        else -> normalForegroundColor
+      },
+    )
+}
 
 @PreviewDark
 @Composable
