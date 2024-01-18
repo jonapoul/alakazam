ade2edc27256df6f1b7a44ab7dd4c6387bf1ac61
Jon Poulton <jpoulton@pm.me>
2024-01-18 18:31:29 +0000

    Make TakContexts a class

--- modules/tak/core/src/main/kotlin/dev/jonpoulton/alakazam/tak/core/TakContexts.kt
+++ modules/tak/core/src/main/kotlin/dev/jonpoulton/alakazam/tak/core/TakContexts.kt
@@ -2,22 +2,15 @@ package dev.jonpoulton.alakazam.tak.core
 
 import android.widget.Toast
 
-public interface TakContexts {
-  public val plugin: PluginContext
-  public val app: AppContext
-}
+public class TakContexts(
+  public val plugin: PluginContext,
+  public val app: AppContext,
+) {
+  public fun toast(message: Int, length: Int = Toast.LENGTH_LONG) {
+    toast(message = plugin.getString(message), length)
+  }
 
-private data class TakContextsImpl(
-  override val plugin: PluginContext,
-  override val app: AppContext,
-) : TakContexts
-
-public fun TakContexts(plugin: PluginContext, app: AppContext): TakContexts = TakContextsImpl(plugin, app)
-
-public fun TakContexts.toast(message: Int, length: Int = Toast.LENGTH_LONG) {
-  toast(message = plugin.getString(message), length)
-}
-
-public fun TakContexts.toast(message: String, length: Int = Toast.LENGTH_LONG) {
-  app.toast(message, length)
+  public fun toast(message: String, length: Int = Toast.LENGTH_LONG) {
+    app.toast(message, length)
+  }
 }
