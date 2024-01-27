a5771236ca7c21069f8d6fe9ac46e46537b0ef4d
Jon Poulton <jpoulton@pm.me>
2024-01-27 14:55:02 +0000

    Fixed button interactions

--- modules/tak/compose/components/src/main/kotlin/dev/jonpoulton/alakazam/tak/compose/button/TakButtonsLegacy.kt
+++ modules/tak/compose/components/src/main/kotlin/dev/jonpoulton/alakazam/tak/compose/button/TakButtonsLegacy.kt
@@ -2,6 +2,7 @@ package dev.jonpoulton.alakazam.tak.compose.button
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -27,13 +28,17 @@ import androidx.compose.material.icons.filled.SwapHoriz
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -41,6 +46,7 @@ import dev.jonpoulton.alakazam.android.ui.compose.EmptyCallback
 import dev.jonpoulton.alakazam.android.ui.compose.PreviewDark
 import dev.jonpoulton.alakazam.kotlin.core.ifTrue
 import dev.jonpoulton.alakazam.tak.compose.core.TakColors
+import dev.jonpoulton.alakazam.tak.compose.core.TakToast
 import dev.jonpoulton.alakazam.tak.compose.preview.TakPreview
 
 @Composable
@@ -140,7 +146,8 @@ public fun TakIconButtonLegacy(
       .clip(shape = RoundedEdges)
       .background(brush = backgroundGradient(isChecked, isDisabled))
       .size(36.dp),
-    onClick = { onClick() },
+    interactionSource = interactionSource,
+    onClick = onClick,
   ) {
     Icon(
       imageVector = icon,
