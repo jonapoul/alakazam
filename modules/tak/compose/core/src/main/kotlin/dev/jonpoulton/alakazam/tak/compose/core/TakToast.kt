9175218c39223331c0739c29cc0d438f95209c01
Jon Poulton <jpoulton@pm.me>
2024-01-28 13:22:56 +0000

    Add TakToast for string resources

--- modules/tak/compose/core/src/main/kotlin/dev/jonpoulton/alakazam/tak/compose/core/TakToast.kt
+++ modules/tak/compose/core/src/main/kotlin/dev/jonpoulton/alakazam/tak/compose/core/TakToast.kt
@@ -1,11 +1,18 @@
 package dev.jonpoulton.alakazam.tak.compose.core
 
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 
 @Composable
 public fun TakToast(text: String, length: Int = Toast.LENGTH_LONG) {
   with(LocalTakContexts.current) {
     Toast.makeText(app, text, length).show()
   }
+}
+
+@Composable
+public fun TakToast(@StringRes id: Int, length: Int = Toast.LENGTH_LONG) {
+  TakToast(stringResource(id), length)
 }
