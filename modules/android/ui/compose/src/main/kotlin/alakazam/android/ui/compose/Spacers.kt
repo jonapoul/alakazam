afcebddb1cf1698e2e2efcbd8636953beabf02a0
Jon Poulton <jpoulton@pm.me>
2024-04-14 21:45:55 +0100

    More spacers

--- modules/android/ui/compose/src/main/kotlin/alakazam/android/ui/compose/Spacers.kt
+++ modules/android/ui/compose/src/main/kotlin/alakazam/android/ui/compose/Spacers.kt
@@ -3,6 +3,8 @@
 package alakazam.android.ui.compose
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -17,6 +19,16 @@ public fun VerticalSpacer(height: Dp = 10.dp) {
 }
 
 @Composable
+public fun ColumnScope.VerticalSpacer(weight: Float) {
+  Spacer(modifier = Modifier.weight(weight))
+}
+
+@Composable
 public fun HorizontalSpacer(width: Dp = 10.dp) {
   Spacer(modifier = Modifier.width(width))
+}
+
+@Composable
+public fun RowScope.HorizontalSpacer(weight: Float) {
+  Spacer(modifier = Modifier.weight(weight))
 }
