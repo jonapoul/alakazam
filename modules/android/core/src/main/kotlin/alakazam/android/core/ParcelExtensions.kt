b98d694324b04be0b88affe50ba224eb0d8ea295
Jon Poulton <jpoulton@pm.me>
2024-06-06 22:05:03 +0100

    Parcelable extension

---
+++ modules/android/core/src/main/kotlin/alakazam/android/core/ParcelExtensions.kt
@@ -0,0 +1,13 @@
+package alakazam.android.core
+
+import android.os.Build
+import android.os.Parcel
+
+public inline fun <reified T> Parcel.readSerializableCompat(): T? {
+  return if (Versions.isAtLeast(Build.VERSION_CODES.TIRAMISU)) {
+    readSerializable(T::class.java.classLoader, T::class.java)
+  } else {
+    @Suppress("DEPRECATION")
+    readSerializable() as? T?
+  }
+}
