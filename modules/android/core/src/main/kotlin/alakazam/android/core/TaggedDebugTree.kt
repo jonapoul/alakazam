b9f8d0cff6707180f0fff19544b94dc72ed6ca95
Jon Poulton <jpoulton@pm.me>
2024-07-28 13:30:04 +0100

    Configurable TaggedDebugTree

--- modules/android/core/src/main/kotlin/alakazam/android/core/TaggedDebugTree.kt
+++ modules/android/core/src/main/kotlin/alakazam/android/core/TaggedDebugTree.kt
@@ -2,12 +2,22 @@ package alakazam.android.core
 
 import timber.log.Timber
 
-public open class TaggedDebugTree(private val prefix: String?) : Timber.DebugTree() {
+public open class TaggedDebugTree(
+  private val prefix: String?,
+  private val includeLineNumber: Boolean = true,
+  private val includeMethodName: Boolean = false,
+) : Timber.DebugTree() {
   override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
     val taggedMessage = if (prefix == null) message else "$prefix: $message"
     super.log(priority, tag, taggedMessage, t)
   }
 
-  override fun createStackElementTag(element: StackTraceElement): String =
-    "(${element.fileName}:${element.lineNumber})#${element.methodName}"
+  override fun createStackElementTag(element: StackTraceElement): String? {
+    val methodName = if (includeMethodName) "#${element.methodName}" else ""
+    return if (includeLineNumber) {
+      "(${element.fileName}:${element.lineNumber})$methodName"
+    } else {
+      "${element.fileName}#${methodName}"
+    }
+  }
 }
