0ee452b631fbd50535e7f57130e04e2c5230d977
Jon Poulton <jpoulton@pm.me>
2024-10-20 11:10:05 +0100

    Bump deps

--- build-logic/src/main/kotlin/convention-style-kotlin.gradle.kts
+++ build-logic/src/main/kotlin/convention-style-kotlin.gradle.kts
@@ -1,10 +1,13 @@
+import blueprint.recipes.DetektAll
 import blueprint.recipes.detektBlueprint
 import blueprint.recipes.ktlintBlueprint
 import org.gradle.accessors.dm.LibrariesForLibs
 
 val libs = the<LibrariesForLibs>()
 
-detektBlueprint()
+detektBlueprint(
+  detektAllConfig = DetektAll.Apply(ignoreRelease = true),
+)
 
 ktlintBlueprint(
   ktlintCliVersion = libs.versions.ktlint.cli,
