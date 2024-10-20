da8b63d102518272b45c7de85371ada51ad7d958
Jon Poulton <jpoulton@pm.me>
2024-10-20 10:55:10 +0100

    Create RunCatching.kt

---
+++ modules/kotlin/core/src/main/kotlin/alakazam/kotlin/core/RunCatching.kt
@@ -0,0 +1,17 @@
+package alakazam.kotlin.core
+
+import kotlinx.coroutines.CancellationException
+
+/**
+ * Same as [kotlin.runCatching], but explicitly handles [CancellationException] as a special case.
+ *
+ * See reference, [Kotlin doc](https://kotlinlang.org/docs/cancellation-and-timeouts.html#cancellation-is-cooperative).
+ */
+@Suppress("RedundantSuspendModifier")
+public suspend fun <R> coRunCatching(block: () -> R): Result<R> = try {
+  Result.success(block())
+} catch (e: CancellationException) {
+  throw e
+} catch (e: Throwable) {
+  Result.failure(e)
+}
