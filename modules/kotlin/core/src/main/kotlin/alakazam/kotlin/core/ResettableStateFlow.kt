d793c9eb8387e299d2af44f3bfa762918b4e93c8
Jon Poulton <jpoulton@pm.me>
2025-02-20 10:32:59 +0000

    Added ResettableStateFlow

---
+++ modules/kotlin/core/src/main/kotlin/alakazam/kotlin/core/ResettableStateFlow.kt
@@ -0,0 +1,18 @@
+@file:Suppress("warnings", "ALL")
+
+package alakazam.kotlin.core
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+
+public interface ResettableStateFlow<T> : MutableStateFlow<T> {
+  public fun reset()
+}
+
+public fun <T> ResettableStateFlow(value: T): ResettableStateFlow<T> = ResettableStateFlowImpl(value)
+
+private class ResettableStateFlowImpl<T>(
+  private val initialValue: T,
+) : ResettableStateFlow<T>, MutableStateFlow<T> by MutableStateFlow(initialValue) {
+  override fun reset() = update { initialValue }
+}
---
+++ modules/kotlin/core/src/test/kotlin/alakazam/kotlin/core/ResettableStateFlowTest.kt
@@ -0,0 +1,29 @@
+package alakazam.kotlin.core
+
+import app.cash.turbine.test
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class ResettableStateFlowTest {
+  @Test
+  fun `Observe then reset`() = runTest {
+    val flow = ResettableStateFlow(value = 123)
+    flow.test {
+      assertEquals(expected = 123, actual = awaitItem())
+
+      flow.update { 456 }
+      assertEquals(expected = 456, actual = awaitItem())
+
+      flow.update { 789 }
+      assertEquals(expected = 789, actual = awaitItem())
+
+      flow.reset()
+      assertEquals(expected = 123, actual = awaitItem())
+
+      expectNoEvents()
+      cancelAndIgnoreRemainingEvents()
+    }
+  }
+}
