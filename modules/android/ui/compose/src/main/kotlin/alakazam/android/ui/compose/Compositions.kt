5173a133019d1b21ea20655d9e95f7a5446319e3
Jon Poulton <jpoulton@pm.me>
2025-03-08 18:11:05 +0000

    Use ReadOnlyComposable

--- modules/android/ui/compose/src/main/kotlin/alakazam/android/ui/compose/Compositions.kt
+++ modules/android/ui/compose/src/main/kotlin/alakazam/android/ui/compose/Compositions.kt
@@ -2,7 +2,9 @@ package alakazam.android.ui.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.ReadOnlyComposable
 
 public val <T> ProvidableCompositionLocal<T?>.currentOrThrow: T
   @Composable
+  @ReadOnlyComposable
   get() = current ?: error("CompositionLocal $this is null")
--- modules/android/ui/compose/src/main/kotlin/alakazam/android/ui/compose/Configurations.kt
+++ modules/android/ui/compose/src/main/kotlin/alakazam/android/ui/compose/Configurations.kt
@@ -2,7 +2,9 @@ package alakazam.android.ui.compose
 
 import android.content.res.Configuration
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalConfiguration
 
 @Composable
+@ReadOnlyComposable
 public fun isLandscape(): Boolean = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
