0b81cb6ecc1e926ef38d00fe96fdd6e4a4ae7cbd
Jon Poulton <jpoulton@pm.me>
2025-10-02 20:18:14 +0100

    Update dependencies

--- build-logic/src/main/kotlin/alakazam/gradle/ConventionPublish.kt
+++ build-logic/src/main/kotlin/alakazam/gradle/ConventionPublish.kt
@@ -4,11 +4,19 @@ import com.vanniktech.maven.publish.MavenPublishPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.dokka.gradle.DokkaPlugin
+import org.jetbrains.dokka.gradle.DokkaTask
 
 class ConventionPublish : Plugin<Project> {
-  override fun apply(target: Project): Unit = with(target.pluginManager) {
-    apply(MavenPublishPlugin::class)
-    apply(DokkaPlugin::class)
+  override fun apply(target: Project): Unit = with(target) {
+    with(pluginManager) {
+      apply(MavenPublishPlugin::class)
+      apply(DokkaPlugin::class)
+    }
+
+    tasks.withType<DokkaTask>().configureEach {
+      failOnWarning.set(true)
+    }
   }
 }
